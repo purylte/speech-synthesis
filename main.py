3599aa764c2656cf43d8aea09cd028edd63c25b0
joshste <josh.steven@ui.ac.id>
2023-04-01 23:29:07 +0700

    second commit

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ from fastapi.responses import JSONResponse
 from worker import start_task
 from pydantic import BaseModel
 from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+origins = [
+    "http://localhost:8080",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class RequestBody(BaseModel):
