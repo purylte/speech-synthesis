1ca62540795b107b54b2e48f1ec2425b73347d7f
joshste <josh.steven@ui.ac.id>
2023-04-01 13:45:32 +0700

    fix endpoint typo

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ class RequestBody(BaseModel):
     output_file_name: str
 
 
-@app.post("api/generate", status_code=201)
+@app.post("/api/generate", status_code=201)
 def run_task(request_body: RequestBody):
     task = start_task.delay(
         request_body.prompt, request_body.speaker_index, request_body.output_file_name)
     return JSONResponse({"task_id": task.id})
 
 
-@app.get("api/tasks/{task_id}")
+@app.get("/api/tasks/{task_id}")
 def get_status(task_id):
     task_result = AsyncResult(task_id)
     if task_result.failed():
